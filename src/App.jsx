8cdf9ab64899203ff4d6f1f5cd2ed05dd8a9b425
unknown <ivanferreyradaniel@gmail.com>
2023-11-16 12:23:55 -0300

    conectar api con foreach

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,49 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './App.css'
+import Boton from './componentes/boton';
 
-const API = "https://pokeapi.co/api/v2/pokemon/ditto";
-
+/* const API = "https://pokeapi.co/api/v2/pokemon/ditto";
+ */
 function App() {
-  const [contador, setContador] = useState(0);
-  const [nombre, setNombre] = useState("def nombre");
-  const [imagen, setImagen] = useState("def imagen");
+
 
   useEffect(() => {
-    async function fetchApi(){
-    const res = await fetch(API);
-    let info = "def info"
-    if(res.ok) {
-      info = await res.json();
-      setNombre(info.name);
-      setImagen (info.sprites.front_default);
+    console.log("Effect");
+
+
+    async function pedirPokemon() {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20  ")
+/* 
+      .then(response => response.json())
+
+        .then(data => console.log(data)) */
+      let listaPokemon = "def 0"
+      if (res.ok)
+
+        listaPokemon = await res.json();
+      console.log(listaPokemon)
+
+      listaPokemon.results.forEach(async pokemon => {
+        
+        const resPoke = await fetch(pokemon.url)
+
+        const infoPoke = await resPoke.json();
+        console.log(infoPoke)
+      });
+
     }
 
-  }
-fetchApi()
+    pedirPokemon()
 
-})
+    return () => {
+      console.log("limpiando efecto")
+    }
+  }, []);
 
-  function aumentar() {
-    setContador(contador + 1);
-    setNombre("otro nombre"+ contador)
-  }
 
   return (
     <>
-      <h1>Hola</h1>
-      <button onClick={aumentar} >clickear</button>
-      <h2>{contador}</h2>
-      <h3>{nombre}</h3>
-      <img src={imagen} />
+      <Boton></Boton>
     </>
   )
 }
